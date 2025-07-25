4372fd93d3821ead4b91b7efee9faa66debc18e9
Ajit <ajitanarase0@gmail.com>
2025-07-25 08:51:27 +0530

    this is my first code

---
+++ simple.java
@@ -0,0 +1,19 @@
+
+/*  Ajit anarase
+ * java syntax
+ * 25-07-2025
+ * public class ClassName{
+
+    
+}
+*/
+import java.io.*;//packckage import
+
+public class simple // class declaration
+{
+    public static void main(String[] args)// main method start
+    {
+        // block of main method
+        System.out.println("this is my first program");
+    }
+}
